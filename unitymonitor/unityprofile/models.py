57f1b414a257af1fe1dd692a5be8dac8e7c37476
김은규 <eungyukm@gmail.com>
2024-05-21 14:22:58 +0900

    모델 데이터에 user nickanme 및 build version 및 system os 추가

--- unitymonitor/unityprofile/models.py
+++ unitymonitor/unityprofile/models.py
@@ -14,7 +14,7 @@ class ProfileData(models.Model):
     device_name = models.CharField(max_length=100, default="none")
     project_name = models.CharField(max_length=100, default="none")
     scene_name = models.CharField(max_length=100, default="none")
-    date = models.CharField(max_length=500)
+    date_time = models.DateTimeField(auto_now_add=True)
     
     fps = models.FloatField(default=0.0)
     min_fps = models.FloatField(default=0.0)
@@ -30,11 +30,15 @@ class ProfileData(models.Model):
     texture_memory = models.FloatField(default=0.0)
     mesh_memory = models.FloatField(default=0.0)
 
+    user_nickname = models.CharField(max_length=100, default="none")
+    build_version = models.CharField(max_length=100, default="none")
+    system_os = models.CharField(max_length=100, default="none")
+
 class ProfileRecordInfo(models.Model):
     profile_record_info_idx = models.AutoField(primary_key=True)
     profile_record_title = models.CharField(max_length=100, default="none")
     profile_record_contents = models.CharField(max_length=500, default="none")
-    date = models.CharField(max_length=500)
+    date_time = models.DateTimeField(auto_now_add=True)
     user_name = models.CharField(max_length=100, default="none")
     profile_record_start_idx = models.IntegerField(default="-1")
     profile_record_end_idx = models.IntegerField(default="-1")
@@ -45,7 +49,7 @@ class ProfileRecord(models.Model):
     device_name = models.CharField(max_length=100, default="none")
     project_name = models.CharField(max_length=100, default="none")
     scene_name = models.CharField(max_length=100, default="none")
-    date = models.CharField(max_length=500)
+    date_time = models.DateTimeField(auto_now_add=True)
     
     fps = models.FloatField(default=0.0)
     min_fps = models.FloatField(default=0.0)
@@ -60,6 +64,10 @@ class ProfileRecord(models.Model):
     system_memory = models.FloatField(default=0.0)
     texture_memory = models.FloatField(default=0.0)
     mesh_memory = models.FloatField(default=0.0)
+
+    user_nickname = models.CharField(max_length=100, default="none")
+    build_version = models.CharField(max_length=100, default="none")
+    system_os = models.CharField(max_length=100, default="none")
 
     profile_record_info = models.ForeignKey(ProfileRecordInfo, on_delete=models.CASCADE, related_name='record_infos')
 
